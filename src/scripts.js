51652aa8cd8bc51bb7cf3736832917c5cc86b78a
Joel Alejandro Espinoza Sánchez <alex.esa18@gmail.com>
2024-01-11 15:43:17 -0600

    Fixing undefined inconsistencies and case when db returns to length = 0

--- src/scripts.js
+++ src/scripts.js
@@ -9,7 +9,7 @@ function randomName()
 
     let girlNames = ['María', 'Andrea', 'Ana', 'Lucía', 'Sofía', 'Martina', 'María', 'Julia', 'Paula', 'Valeria', 'Emma', 'Daniela', 'Carla', 'Alba', 'Noa', 'Alma', 'Sara', 'Carmen', 'Vega', 'Lara', 'Mia', 'Valentina', 'Olivia', 'Claudia', 'Jimena', 'Lola', 'Chloe', 'Aitana', 'Abril', 'Ana', 'Laia', 'Triana', 'Candela', 'Alejandra', 'Elena', 'Vera', 'Manuela', 'Adriana', 'Inés', 'Marta', 'Carlota', 'Irene', 'Victoria', 'Blanca', 'Marina', 'Laura', 'Rocío', 'Alicia', 'Clara', 'Nora', 'Lia', 'Ariadna', 'Zoe', 'Amira', 'Gala', 'Celia', 'Leire', 'Eva', 'Ángela', 'Andrea', 'África', 'Luna', 'Ainhoa', 'Ainara', 'India', 'Nerea', 'Ona', 'Elsa', 'Isabel', 'Leyre', 'Gabriela', 'Aina', 'Cayetana', 'Iria', 'Jana', 'Mar', 'Cloe', 'Lina', 'Julieta', 'Adara', 'Naia', 'Iris', 'Nour', 'Mara', 'Helena', 'Yasmín', 'Natalia', 'Arlet', 'Diana', 'Aroa', 'Amaia', 'Cristina', 'Nahia', 'Isabella', 'Malak', 'Elia', 'Carolina', 'Berta', 'Fátima', 'Nuria', 'Azahara', 'Macarena', 'Aurora'];
     let boyNames = ['Juan', 'Antonio', 'Pedro', 'Hugo', 'Mateo', 'Martín', 'Lucas', 'Leo', 'Daniel', 'Alejandro', 'Manuel', 'Pablo', 'Álleto', 'Adrián', 'Enzo', 'Mario', 'Diego', 'David', 'Oliver', 'Marcos', 'Thiago', 'Marco', 'Álex', 'Javier', 'Izan', 'Bruno', 'Miguel', 'Antonio', 'Gonzalo', 'Liam', 'Gael', 'Marc', 'Carlos', 'Juan', 'Ángel', 'Dylan', 'Nicolás', 'José', 'Sergio', 'Gabriel', 'Luca', 'Jorge', 'Darío', 'Íker', 'Samuel', 'Eric', 'Adam', 'Héctor', 'Francisco', 'Rodrigo', 'Jesús', 'Erik', 'Amir', 'Jaime', 'Ian', 'Rubén', 'Aarón', 'Iván', 'Pau', 'Víctor', 'Guillermo', 'Luis', 'Mohamed', 'Pedro', 'Julen', 'Unai', 'Rafael', 'Santiago', 'Saúl', 'Alberto', 'Noah', 'Aitor', 'Joel', 'Nil', 'Jan', 'Pol', 'Raúl', 'Matías', 'Martí', 'Fernando', 'Andrés', 'Rayan', 'Alonso', 'Ismael', 'Asier', 'Biel', 'Ander', 'Aleix', 'Axel', 'Alan', 'Ignacio', 'Fabio', 'Neizan', 'Jon', 'Teo', 'Isaac', 'Arnau', 'Luka', 'Max', 'Imran', 'Youssef', 'Anas', 'Elías'];
-    let lastNames = ['García', 'Alba', 'Esparza', 'Gómez', 'Velázquez', 'Hernández', 'García', 'Martínez', 'López', 'González', 'Pérez', 'Rodríguez', 'Sánchez', 'Ramírez', 'Cruz', 'Gómez', 'Flores', 'Morales', 'Vázquez', 'Jiménez', 'Reyes', 'Díaz', 'Torres', 'Gutiérrez', 'Ruiz', 'Mendoza', 'Aguilar', 'Méndez', 'Moreno', 'Ortíz', 'Juárez', 'Castillo', 'Álletez', 'Romero', 'Ramos', 'Rivera', 'Chávez', 'De la Cruz', 'Domínguez', 'Guzmán', 'Velázquez', 'Santiago', 'Herrera', 'Castro', 'letgas', 'Medina', 'Rojas', 'Muñóz', 'Luna', 'Contreras', 'Bautista', 'Salazar', 'Ortega', 'Guerrero ', 'Estrada'];
+    let lastNames = ['García', 'Alba', 'Esparza', 'Gómez', 'Velázquez', 'Hernández', 'García', 'Martínez', 'López', 'González', 'Pérez', 'Rodríguez', 'Sánchez', 'Ramírez', 'Cruz', 'Gómez', 'Flores', 'Morales', 'Vázquez', 'Jiménez', 'Reyes', 'Díaz', 'Torres', 'Gutiérrez', 'Ruiz', 'Mendoza', 'Aguilar', 'Méndez', 'Moreno', 'Ortíz', 'Juárez', 'Castillo', 'Álletez', 'Romero', 'Ramos', 'Rivera', 'Chávez', 'De la Cruz', 'Domínguez', 'Guzmán', 'Velázquez', 'Santiago', 'Herrera', 'Castro', 'Medina', 'Rojas', 'Muñóz', 'Luna', 'Contreras', 'Bautista', 'Salazar', 'Ortega', 'Guerrero ', 'Estrada'];
 
     let gender = Math.floor(Math.random() * (1 - 0 + 1) + 0);
     let nameQuantity = Math.floor(Math.random() * (1 - 0 + 1) + 0);
@@ -20,11 +20,11 @@ function randomName()
     {
         if(gender == 0)
         {
-            nameNumber = Math.floor(Math.random() * (girlNames.length - 0 + 1) + 0);
+            nameNumber = Math.floor(Math.random() * (girlNames.length) + 0);
 
             while(nameNumber == previousNumber)
             {
-                nameNumber = Math.floor(Math.random() * (girlNames.length - 0 + 1) + 0);
+                nameNumber = Math.floor(Math.random() * (girlNames.length) + 0);
             }
 
             previousNumber = nameNumber;
@@ -33,11 +33,11 @@ function randomName()
         }
         if(gender == 1)
         {
-            nameNumber = Math.floor(Math.random() * (boyNames.length - 0 + 1) + 0);
+            nameNumber = Math.floor(Math.random() * (boyNames.length) + 0);
 
             while(nameNumber == previousNumber)
             {
-                nameNumber = Math.floor(Math.random() * (boyNames.length - 0 + 1) + 0);
+                nameNumber = Math.floor(Math.random() * (boyNames.length) + 0);
             }
 
             previousNumber = nameNumber;
@@ -46,8 +46,11 @@ function randomName()
         }
     }
 
-    name = name + lastNames[Math.floor(Math.random() * (lastNames.length - 0 + 1) + 0)] + ' ';
-    name = name + lastNames[Math.floor(Math.random() * (lastNames.length - 0 + 1) + 0)];
+    let lastNamesNumber = Math.floor(Math.random() * (lastNames.length) + 0)
+    name = name + lastNames[lastNamesNumber] + ' ';
+
+    lastNamesNumber = Math.floor(Math.random() * (lastNames.length) + 0)
+    name = name + lastNames[lastNamesNumber];
 
     return name
 }
@@ -256,6 +259,19 @@ buttonAddForm[0].addEventListener("click", function()
                 let row = newRemoveTable.parentNode.parentNode.childNodes[i];
                 row.remove();
                 db.splice(i - 2, 1);
+
+                // Showing (if necessary) the empty registers warning
+                if(db.length == 0)
+                {
+                    let noRegistersDiv = document.getElementsByClassName("noRegistersDiv");
+                    noRegisters = noRegistersDiv[0];
+                    noRegisters.style.display = "block";
+
+                    let divTable = document.getElementsByClassName("divTable");
+                    table = divTable[0];
+                    table.style.display = "none";
+                }
+                
                 break;
             }
         }
